96d4cbd39c51c8f82ab62e5926f5d9425cf64962
Josh Morrow <jcmorrow@thoughtbot.com>
2016-08-26 23:54:36 -0700

    Install timex for dates

--- mix.exs
+++ mix.exs
@@ -47,7 +47,8 @@ defmodule CoffeeSpoons.Mixfile do
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
      {:cowboy, "~> 1.0"},
-     {:wallaby, "~> 0.11.1"}]
+     {:wallaby, "~> 0.11.1"},
+     {:timex, "~> 3.0"}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
